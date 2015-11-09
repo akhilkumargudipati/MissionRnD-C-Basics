fd1f7c712a75b7ff149c5539672655c16158978c
akhilkumargudipati <akhilkumar1996.gudipati@gmail.com>
2015-11-09 22:35:12 +0530

    Submit Commit

--- src/count.cpp
+++ src/count.cpp
@@ -11,6 +11,15 @@ ERROR CASES: Return -1 in error cases.
 
 NOTES:
 */
-int count(int num1, int num2) {
-	return 0;
+int count(int num1, int num2) 
+{
+	int count = 0,iterator;
+	if (num1==0)
+		return -1;
+	for (iterator = num1; iterator <= num2; iterator++)
+	{
+		if (iterator%num1 == 0)
+			count++;
+	}
+	return count;
 }
